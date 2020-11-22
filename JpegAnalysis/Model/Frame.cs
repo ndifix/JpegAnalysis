0f5e4b46d512edfe8a1a98c2111bccc71b1d8a66
ndifix <yoshiki.linuxmint@gmail.com>
2020-11-22 18:14:09 +0900

    Add project files.

---
+++ JpegAnalysis/Model/Frame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JpegAnalysis.Model
+{
+	class Frame
+	{
+		private Task readTask;
+
+		~Frame () {
+			if (readTask != null)
+			{
+				readTask.Wait();
+			}
+		}
+
+		public void ReadFrame(string inPath)
+		{
+			readTask = ReadFrameAsyncImpl(inPath);
+		}
+
+		private async Task ReadFrameAsyncImpl(string inPath)
+		{
+			if (!File.Exists(inPath))
+			{
+				throw new Exception($"存在しないファイルパスです。:{inPath}");
+			}
+
+			byte[] binary;
+			using (var fs = new FileStream(inPath, FileMode.Open))
+			{
+				binary = new byte[fs.Length];
+				await fs.ReadAsync(binary, 0, (int)fs.Length);
+			}
+		}
+	}
+}
---
+++ JpegAnalysis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using JpegAnalysis.Model;
+
+namespace JpegAnalysis
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length != 1)
+			{
+				Console.WriteLine("invalid argument");
+				return;
+			}
+
+			string inPath = args[0];
+			Frame frame = new Frame();
+			frame.ReadFrame(inPath);
+		}
+	}
+}
